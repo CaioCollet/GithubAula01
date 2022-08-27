354dc24020014d8f3f67e48eca0bffe0aa957156
Caio Felipe <Caio.collet@alunos.sc.senac.br>
2022-08-26 21:03:59 -0300

    nova classe carro e nova impressao

--- src/carro.java
+++ src/carro.java
@@ -1,24 +1,22 @@
+
+import java.util.Scanner;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package githubeaula1;
 
 /**
  *
  * @author Suporte
  */
-public class GithubeAula1 {
-
-    /**
-     * @param args the command line arguments
-     */
+public class carro {
     public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("hello World!");
-        System.out.println("Bem vindo ao Git ");
+        Scanner leia =new Scanner(System.in);
+        
         
     }
+ 
 
 }
